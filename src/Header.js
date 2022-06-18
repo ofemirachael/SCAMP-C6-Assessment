d2a07806bf9ff2845af40944d9aa97d0abf32ff6
Femi Olayode <folayode@wragbysolutions.com>
2022-06-18 21:42:06 +0100

    completed resource

--- src/Header.js
+++ src/Header.js
@@ -2,26 +2,37 @@ import React from "react";
 import './Header.css';
 import { ShoppingCart } from 'react-feather';
 import profile from "./img/profile.jpg";
+import bc from "./img/bc.png";
 
+// let navi = document.querySelector(".mainnav");
+// function onImageClick()  {
+//   console.log("nav click");
+//   navi.style.display = "block";
+// }
 
 class Header extends React.Component{
 
-    render() {
+  state = { showing: true };
 
+    render() {
+      const { showing } = this.state;
         return <>
         <div className="navigation">
            
             <nav className="headerspan">
+                <a href="#" className="toggle"  onClick={() => this.setState({ showing: !showing })}><img src={bc} height="24"/></a>
                 <a className="logo">Sneakers</a>
-
-                <ul className="mainnav">
-                    <li className="item">Collections</li>
-                    <li className="item">Men</li>
-                    <li className="item">Women</li>
-                    <li className="item">About</li>
-                    <li className="item">Contact</li>
-                    <li className="toggle"><a href="#"><img src="./img/images.png" /></a></li>
-                </ul>
+                
+                    <ul className="mainnav" style={{ display: (!showing ? 'block' : 'none') }}>
+                        <li className="item">Collections</li>
+                        <li className="item">Men</li>
+                        <li className="item">Women</li>
+                        <li className="item">About</li>
+                        <li className="item">Contact</li>
+                        
+                    </ul>
+                  
+               
 
             
             </nav>
--- src/ImageCarousel.js
+++ src/ImageCarousel.js
@@ -7,23 +7,14 @@ import imagefour from "./img/shoefour.jpg";
 
 const images = [ imageone, imagetwo, imagethree, imagefour ]   
 
-let imgone = document.getElementById('Imagesone');
-let imgtwo = document.getElementById('Imagestwo');
-let imgthree = document.getElementById('Imagesthree');
-let imgfour = document.getElementById('Imagesfour');
-
-
 class ImageCarousel extends React.Component{
     
     state = {   shown: imagefour }
-    
     
     onImageClick = (image, key) => {
         this.setState({ shown: image })
        
    }
-   
-
     showPics = images.map((image, key) => {
         
         return (<div key={key} onClick={() => this.onImageClick(image, key)}><img src={image}  className="miniimg" /></div>);
